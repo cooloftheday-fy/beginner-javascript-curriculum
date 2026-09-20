b3390f9b4d1256949e519cf36865664148f613a2
cooloftheday-fy <kityan7273@gmail.com>
2026-09-20 01:29:55 +0000

    done

--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -1,25 +1,38 @@
 // Problem 1
 // Create a variable for your lucky number and print it.
-
+const luckyNumber = 5;
+console.log(luckyNumber);
 
 
 // Problem 2
 // Create a variable for the number of hours you sleep and print it.
+const hoursOfSleep = 8;
+console.log(hoursOfSleep);
 
 
 
 // Problem 3
 // Create a variable for your favorite fruit and print it in a full sentence.
+const favoriteFruit = "apples";
+console.log(`My favorite fruit is ${favoriteFruit}.`);
 
 
 
 // Problem 4
 // Create two variables: one for your city and one for your country.
 // Print them on 2 separate lines.
+const city = "Seattle";
+const country = "the United States";
+console.log(city);
+console.log(country);
 
 
 
 // Problem 5
 // Create 3 variables: your pet's name, its age, and its type.
 // Print them on the same line in a full sentence.
+const petName = "Sparkle";
+const petAge = 1;
+const petType = "labrador retriever";
+console.log(`${petName} is a ${petAge}-year-old ${petType}.`);
 
